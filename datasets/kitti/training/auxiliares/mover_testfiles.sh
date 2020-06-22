42e53a736df8f03fc9084841d1b9470462188799
Alejandro Simón Rodríguez <37041719+alexsimonrod@users.noreply.github.com>
2020-06-22 10:26:21 +0200

    archivo inutil

--- datasets/kitti/training/auxiliares/mover_testfiles.sh
+++
@@ -1,7 +0,0 @@
-for i in $(seq -f "%06g" 6000 7480); do
-    mv $i.png /scratch/tfg-adas/datasets/kitti/training/image_test
-done
-
-#for i in $(seq -f "%06g" 5000 5999); do
-#    mv $i.txt /scratch/tfg-adas/datasets/kitti/training/label_2
-#done
